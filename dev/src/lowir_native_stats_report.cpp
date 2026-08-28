44df85f37f240bdeb1e812d421a0fc04ed8b5952
Vishvananda Abrams <vishvananda@gmail.com>
2026-08-28 13:21:25 +0000

    driver: keep shared native stats reporting near callers

--- dev/src/lowir_native_stats_report.cpp
+++ dev/src/lowir_native_stats_report.cpp
@@ -189,24 +189,6 @@ void report_edge_staging_stats(std::ostream & output, const Stats & stats)
   }
 }
 
-void report_codegen_result_stats(std::ostream & output, const Stats & stats)
-{
-  output << " narrow_call_result_normalizations_omitted="
-         << stats.narrow_call_result_normalizations_omitted
-         << " redundant_integer_normalizations_omitted="
-         << stats.redundant_integer_normalizations_omitted
-         << " fused_integer_normalization_moves="
-         << stats.fused_integer_normalization_moves
-         << " scratch_carried_reloads=" << stats.scratch_carried_reloads;
-  report_code_shape_stats(output, stats);
-  report_edge_staging_stats(output, stats);
-  output << " shared_storage_lifetime_extensions="
-         << stats.shared_storage_lifetime_extensions
-         << " reclaim_attempts=" << stats.reclaim_attempts
-         << " reclaim_parameter_visits=" << stats.reclaim_parameter_visits
-         << " reclaims=" << stats.reclaims;
-}
-
 void report_function_census(std::ostream & output, const Stats & stats)
 {
   for(std::size_t i = 0; i < stats.function_census_lines.size(); ++i)
--- dev/src/lowir_native_stats_report.h
+++ dev/src/lowir_native_stats_report.h
@@ -2,7 +2,7 @@
 
 #include "lowir_native.h"
 
-#include <iosfwd>
+#include <ostream>
 
 namespace lowir_native {
 
@@ -12,8 +12,24 @@ void report_codegen_pipeline_stats(std::ostream & output,
   const Stats & stats);
 void report_code_shape_stats(std::ostream & output, const Stats & stats);
 void report_edge_staging_stats(std::ostream & output, const Stats & stats);
-void report_codegen_result_stats(std::ostream & output,
-  const Stats & stats);
+inline void report_codegen_result_stats(
+    std::ostream & output, const Stats & stats)
+{
+  output << " narrow_call_result_normalizations_omitted="
+         << stats.narrow_call_result_normalizations_omitted
+         << " redundant_integer_normalizations_omitted="
+         << stats.redundant_integer_normalizations_omitted
+         << " fused_integer_normalization_moves="
+         << stats.fused_integer_normalization_moves
+         << " scratch_carried_reloads=" << stats.scratch_carried_reloads;
+  report_code_shape_stats(output, stats);
+  report_edge_staging_stats(output, stats);
+  output << " shared_storage_lifetime_extensions="
+         << stats.shared_storage_lifetime_extensions
+         << " reclaim_attempts=" << stats.reclaim_attempts
+         << " reclaim_parameter_visits=" << stats.reclaim_parameter_visits
+         << " reclaims=" << stats.reclaims;
+}
 void report_function_census(std::ostream & output, const Stats & stats);
 
 }
